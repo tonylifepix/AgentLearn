3dc41c8b306ed4e74c0afca0351c871d92f85419
Shehtab <szaman5@binghamton.edu>
2025-09-12 09:56:50 -0400

    Add some scaffolding for the agent loop

--- agent_loop.py
+++ agent_loop.py
@@ -17,7 +17,7 @@ server_params = StdioServerParameters(
 )
 
 
-async def main():
+async def main(run_iteration=1):
 
     async with stdio_client(server_params) as (read, write):
         async with ClientSession(read, write) as session:
@@ -42,7 +42,6 @@ async def main():
                 + " The final output should just be a list of the unique molecules"
                 + " in the format [smiles1, smiles2, smiles3, smiles4, smiles5] \n\n"
             )
-            # chat_prompt = "What color are Zebras? \n\n"
             print(chat_prompt)
             prompt = chat_prompt
 
@@ -65,3 +64,48 @@ async def main():
             print("Thoughts tokens:", response.usage_metadata.thoughts_token_count)
             print("\n\n")
             print("Response:\n", response.text)
+
+            print("\n\n")
+            new_mols = []
+            assert response.candidates is not None
+            assert len(response.candidates) > 0
+            assert response.candidates[0] is not None
+            assert response.candidates[0].content is not None
+            assert response.candidates[0].content.parts is not None
+            for part in response.candidates[0].content.parts:
+                if not part.text:
+                    continue
+                if part.thought:
+                    print("Thought:", part.thought.text)
+                if part.tool_call:
+                    print("Tool call:", part.tool_call)
+                if part.tool_response:
+                    print("Tool response:", part.tool_response)
+                if part.text:
+                    print("Text:", part.text)
+                    new_mols.append(part.text)
+            print("Response text:", response.text)
+            print("Response usage metadata:", response.usage_metadata)
+            print("New molecules:", new_mols)
+            print(
+                "New molecules list:",
+                [mol.strip() for mol in new_mols if mol.strip().startswith("C")],
+            )
+            print("New molecules count:", len(new_mols))
+            print("New molecules unique count:", len(set(new_mols)))
+            print("New molecules unique list:", set(new_mols))
+            return new_mols
+
+
+if __name__ == "__main__":
+    for i in range(10):
+        # Obtained new molecules from the model
+        new_mol_to_use = asyncio.run(main(run_iteration=i))
+
+        # Retrain the model with the new molecules
+        # retrain_model(new_mol_to_use)  # This function needs to be defined
+
+        # Save the model
+        # torch.save(model.state_dict(), f"smiles_model.pt")
+
+        # regenerate the latent vectors for the entire training set
